f61cdec7ef7fda1c345ac9a6a69010f738e79b1e
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2025-02-08 14:00:16 +0000

    better test

--- src/test/scala/validation/CSVValidatorTest.scala
+++ src/test/scala/validation/CSVValidatorTest.scala
@@ -10,14 +10,17 @@ class CSVValidatorTest extends AnyFunSuite:
     val jsonConfigFileName = "organisationBase.json"
     val altKey = "tdrFileHeader"
     val idKey = "Filepath"
-    val params = Parameters(jsonConfigFileName, List(jsonConfigFileName, "openRecord.json"), Some(altKey), "sample.csv", Some(idKey), Some(jsonConfigFileName),Some(altKey))
+    val params = Parameters(jsonConfigFileName, List(jsonConfigFileName, "openRecord.json"), Some(altKey), "sample.csv", Some(idKey), Some(jsonConfigFileName), Some(altKey))
 
     import cats.effect.unsafe.implicits.*
     val runMe = CSVFileValidationLambdaHandler.csvFileValidation(params).unsafeRunSync()
     runMe match
-      case Valid(data) => println(data)
-        fail("Should have failed")
+      case Valid(data) => fail("Should have failed")
       case Invalid(error) =>
+        assert(error.head.assetId equals "test/test2.txt")
+        assert(error.head.errors.toList.head.errorKey equals "enum")
+        assert(error.head.errors.toList.head.value equals "OpenX")
+        assert(error.head.errors.toList.head.property equals "Closure status")
         assert(error.toList.size == 2)
 
   }
